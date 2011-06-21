5ae5e4bc710b64c2c0eaf79234c1a7a39f61568f
Max Gonzih <gonzih@gmail.com>
2011-06-21 21:35:10 +0300

    fix

--- .init.sh
+++ .init.sh
@@ -17,7 +17,7 @@ echo "====================================> Installing vundle"
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 echo "====================================> Running vundle"
-vim -c "execute \"BundleInstall!\" | q"
+vim -c "execute 'BundleInstall!' | q"
 
 echo "====================================> Installing oh_my_zsh"
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
