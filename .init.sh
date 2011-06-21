397eb175b48c145db20277e8d0ce1f971b82fdc0
Max Gonzih <gonzih@gmail.com>
2011-06-21 18:00:36 +0300

    hide all output

--- .init.sh
+++ .init.sh
@@ -1,26 +1,26 @@
 cd ~
 
 echo "====================================> Installing dotfiles"
-git clone git@github.com:Gonzih/dotfiles.git
-mv ~/dotfiles/* -t ~/ -f
-mv ~/dotfiles/.* -t ~/ -f
-rm ~/dotfiles -rf
+git clone git@github.com:Gonzih/dotfiles.git &> /dev/null
+mv ~/dotfiles/* -t ~/ -f &> /dev/null
+mv ~/dotfiles/.* -t ~/ -f &> /dev/null
+rm ~/dotfiles -rf &> /dev/null
 
 echo "====================================> Installing submodules"
-git submodule init
-git submodule update
+git submodule init &> /dev/null
+git submodule update &> /dev/null
 
 echo "====================================> Installing vundle"
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle &> /dev/null
 
 echo "====================================> Running vundle"
-vim -c "execute \"BundleInstall!\" | q"
+vim -c "execute \"BundleInstall!\" | q" &> /dev/null
 
 echo "====================================> Installing oh_my_zsh"
-wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh &> /dev/null
 
 echo "====================================> Installing rvm"
-sh < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+sh < <(curl -s https://rvm.beginrescueend.com/install/rvm) &> /dev/null
 
 echo "====================================> Launch zsh"
 zsh
