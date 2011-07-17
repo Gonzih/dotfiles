e06bb3750efbd35b679e0f8d164c1ac7f06bc4f1
Max Gonzih <gonzih@gmail.com>
2011-07-17 15:15:34 +0300

    gmailnotify in conky

---
+++ scripts/gmailnotify.rb
@@ -0,0 +1,11 @@
+begin
+  require 'gmail'
+
+  password = `cat ~/.gmail.password`.chomp
+  gmail = Gmail.connect 'gonzih@gmail.com', password
+  new_count = gmail.inbox.count(:unread).to_i
+  puts "#{new_count > 0 ? "#{new_count} new" : 'No new'} email#{new_count > 1 || new_count == 0 ? 's' : nil}"
+  gmail.logout
+resque LoadError
+  puts 'sudo gem install gmail'
+end
