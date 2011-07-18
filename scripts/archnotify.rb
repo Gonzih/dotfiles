a276e51e781a86313d297f381cd3b5c22398e780
Max Gonzih <gonzih@gmail.com>
2011-07-18 17:27:15 +0300

    Revert "1 package and 2 packages"
    This reverts commit 3a487310e2aadbcfaabef26c672c7eb563dc1759.

--- scripts/archnotify.rb
+++ scripts/archnotify.rb
@@ -1,2 +1,2 @@
 data = `pacman -Qu`
-puts data.lines.count > 0 ? "#{data.lines.count} package#{data.lines.count > 1 ? 's' : nil}" : "System is up-to-date"
+puts data.lines.count > 0 ? "#{data.lines.count} packages" : "System is up-to-date"
