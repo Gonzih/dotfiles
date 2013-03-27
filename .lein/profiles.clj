6fe361a814d4b90ef89a3ea345e595d90d1adac7
Max Gonzih <gonzih@gmail.com>
2013-03-27 21:31:29 +0300

    Add .lein/profiles.clj file

---
+++ .lein/profiles.clj
@@ -0,0 +1,7 @@
+{:user {:plugins [[lein-swank "1.4.0"]
+                  [lein-pprint "1.1.1"]
+                  [lein-noir "1.2.1"]
+                  [lein-exec "0.3.0"]
+                  [lein-drip "0.1.1-SNAPSHOT"]
+                  [lein-cljsbuild "0.3.0"]
+                  [lein-kibit "0.0.7"]]}}
