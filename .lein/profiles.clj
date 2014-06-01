d421d2418d5a4acabafda03eadb2d385893a02ff
Max Gonzih <gonzih@gmail.com>
2014-06-01 10:20:34 +0200

    Add errors helper to lein configuration

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,6 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
                   [lein-exec "0.3.0"]
                   [lein-drip "0.1.1-SNAPSHOT"]
-                  ;[lein-cljsbuild "0.3.2"]
                   [lein-kibit "0.0.8"]
                   [lein-simpleton "1.1.0"]
                   [lein-ancient "0.4.3"]
@@ -10,11 +9,13 @@
                   [lein-ritz "0.7.0"]
                   [hiccup-bridge "1.0.0-SNAPSHOT"]
                   [lein-gorilla "0.1.2"]]
-        ;:injections  [(require '[spyscope core]
-                               ;'[redl core complete])]
-        ;:dependencies [[spyscope "0.1.3"]
-                       ;[redl "0.1.0"]]
+
+        :dependencies [[clj-stacktrace "0.2.7"]]
+        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                            'print-cause-trace)
+                           new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                           'pst)]
+                       (alter-var-root orig (constantly (deref new))))]
 
         :repl-options {:prompt (fn [ns] (str "\u001B[35m[\u001B[34m" ns "\u001B[35m]\u001B[33mcλ:\u001B[m " ))
-                       :welcome (println "Time for  REPL Driven Development!")}
-        }}
+                       :welcome (println "Time for  REPL Driven Development!")}}}
