50053c5eb66ca28d3531757127995e0d20f491ac
Max Gonzih <gonzih@gmail.com>
2014-11-18 10:34:24 +0100

    Add nixpkgs configuration

---
+++ .nixpkgs/config.nix
@@ -0,0 +1,20 @@
+{
+  allowUnfree = true;
+  packageOverrides = pkgs : with pkgs; {
+    rvmEnv = pkgs.myEnvFun {
+      name = "rvm";
+      buildInputs = [ stdenv which readline zlib openssl libxml2 libxslt libiconv mysql ];
+      extraCmds = ''
+        unset http_proxy
+        unset ftp_proxy
+        export LIBXML2_DIR=${libxml2}
+        export LIBXSLT_DIR=${libxslt}
+        export LIBICONV_DIR=${libiconv}
+        export OPENSSL_DIR=${openssl}
+        export READLINE_DIR=${readline}
+        bash --login
+        rvm use ruby
+      '';
+    };
+  };
+}
