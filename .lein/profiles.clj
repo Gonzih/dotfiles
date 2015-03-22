1e6b9176e91e332ddf5a67fccc9d2f4cd936c165
Max Gonzih <gonzih@gmail.com>
2015-03-22 20:22:28 +0100

    Cleanup lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,20 +1,9 @@
-{:user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-exec "0.3.0"]
-                  [lein-drip "0.1.1-SNAPSHOT"]
-                  [lein-kibit "0.0.8"]
+{:user {:plugins [[lein-kibit "0.0.8"]
                   [lein-simpleton "1.1.0"]
                   [lein-ancient "0.4.3"]
                   [lein-try "0.4.0"]
-                  [lein-ritz "0.7.0"]
-                  [lein-gorilla "0.3.3"]
-                  [cider/cider-nrepl "0.8.1"]]
-
-        :dependencies [[clj-stacktrace "0.2.7"]]
-        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                            'print-cause-trace)
-                           new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                           'pst)]
-                       (alter-var-root orig (constantly (deref new))))]
-
+                  [lein-gorilla "0.3.3"]]
+        :dependencies [[cljfmt "0.1.10" :exclusions [org.clojure/clojure]]
+                       [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]
         :repl-options {:prompt (fn [ns] (str "\u001B[35m[\u001B[34m" ns "\u001B[35m]\u001B[33mcλ:\u001B[m " ))
                        :welcome (println "Time for  REPL Driven Development!")}}}
