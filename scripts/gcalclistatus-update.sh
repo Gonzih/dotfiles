156f40b00274782581919aef446d99a9350eb0c4
Max Gonzih <gonzih@gmail.com>
2015-06-30 11:53:42 +0200

    Use 24 hours

--- scripts/gcalclistatus-update.sh
+++ scripts/gcalclistatus-update.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gcalcli --nostarted --calendar "" agenda --nocolor --tsv | awk 'BEGIN { FS="\t" } { print $2, $5 }' > $HOME/.gcalclistatus
+gcalcli --military --nostarted --calendar "" agenda --nocolor --tsv | awk 'BEGIN { FS="\t" } { print $2, $5 }' > $HOME/.gcalclistatus
