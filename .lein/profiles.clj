f33a9c7fda159f07581f0717b364baebfe912896
Max Gonzih <gonzih@gmail.com>
2016-01-20 11:31:22 +0100

    Deps and stuff

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]
-                  [lein-simpleton "1.1.0"]]
+                  [lein-simpleton "1.1.0"]
+                  [lein-try "0.4.3"]]
         :dependencies []
         :repl-options {:prompt (fn [ns] (str "\u001B[35m[\u001B[34m" ns "\u001B[35m]\u001B[33mcλ:\u001B[m " ))
                        :welcome (println "Time for  REPL Driven Development!")}}}
