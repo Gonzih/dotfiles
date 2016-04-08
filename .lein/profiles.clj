77c2c0719729b0a2538fc041d220a48a8224c48d
Max Gonzih <gonzih@gmail.com>
2016-04-08 11:41:46 +0200

    Bump lein ancient version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
-                  [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]
+                  [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
                   [lein-simpleton "1.1.0"]
                   [lein-try "0.4.3"]]
         :dependencies []
