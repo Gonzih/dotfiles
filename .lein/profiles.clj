222a88e4f34c56d958bf3d22ebb9de486d8c2429
Max Gonzih <gonzih@gmail.com>
2016-06-15 09:54:17 +0200

    Add gorilla repl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,8 @@
 {:user {:plugins [[lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
                   [lein-simpleton "1.1.0"]
-                  [lein-try "0.4.3"]]
+                  [lein-try "0.4.3"]
+                  [lein-gorilla "0.3.6"]]
         :dependencies []
         :repl-options {:prompt (fn [ns] (str "\u001B[35m[\u001B[34m" ns "\u001B[35m]\u001B[33mcλ:\u001B[m " ))
                        :welcome (println "Time for  REPL Driven Development!")}}}
