2f50df0ea955a12eb219eb673b8241594b074078
Max Gonzih <gonzih@gmail.com>
2016-08-08 13:42:14 +0200

    Provision all dotfiles via ansible now

--- .init.sh
+++ .init.sh
@@ -6,22 +6,10 @@ mv ~/dotfiles/.* -t ~/ -f
 rm ~/dotfiles -rf
 git reset --hard
 
-git clone --recursive https://github.com/syl20bnr/spacemacs ~/.emacs.d
-
-git clone git@github.com:Gonzih/.vim.git ~/.vim
-
-git clone git@github.com:Gonzih/.mc.git ~/.config/mc
-git clone git@github.com:Gonzih/.fish.git ~/.config/fish
-git clone git@github.com:Gonzih/.xmonad.git ~/.xmonad
-git clone git@github.com:Gonzih/.mutt.git ~/.mutt
+sudo ansible-playbook provision.yml
 
 sh ~/.vim/init.sh
-sh ~/.mutt/init.sh
-
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+# sh ~/.mutt/init.sh
 
 cd ~/.vim/bundle/vimproc.vim && make && cd
 vim +NeoBundleInstall +qall
