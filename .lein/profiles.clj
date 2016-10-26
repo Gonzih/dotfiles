9a8f3810bc22c69535a1c1f1922b253b220845b1
Max Gonzih <gonzih@gmail.com>
2016-10-26 08:29:22 +0200

    Add cider nrepl plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,7 +3,8 @@
                   [lein-plz "0.4.0-SNAPSHOT"]
                   [lein-simpleton "1.1.0"]
                   [lein-try "0.4.3"]
-                  [lein-gorilla "0.3.6"]]
+                  [lein-gorilla "0.3.6"]
+                  [cider/cider-nrepl "0.14.0"]]
         :dependencies []
         :repl-options {:prompt (fn [ns] (str "\u001B[35m[\u001B[34m" ns "\u001B[35m]\u001B[33mcλ:\u001B[m " ))
                        :welcome (println "Time for  REPL Driven Development!")}}}
