3e624172757589d9741fccdb23270e0965b6d2ec
Max Gonzih <gonzih@gmail.com>
2016-11-11 10:33:29 +0100

    Bump cider version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,7 +4,7 @@
                   [lein-simpleton "1.1.0"]
                   [lein-try "0.4.3"]
                   [lein-gorilla "0.3.6"]
-                  [cider/cider-nrepl "0.14.0"]]
+                  [cider/cider-nrepl "0.15.0-SNAPSHOT"]]
         :dependencies []
         :repl-options {:prompt (fn [ns] (str "\u001B[35m[\u001B[34m" ns "\u001B[35m]\u001B[33mcλ:\u001B[m " ))
                        :welcome (println "Time for  REPL Driven Development!")}}}
