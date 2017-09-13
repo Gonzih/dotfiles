85cf9d647eefacd4163bd9903afe7e6c95641f49
Max Gonzih <gonzih@gmail.com>
2017-09-13 11:03:38 +0200

    Fuck cider man

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,8 +3,7 @@
                   [lein-plz "0.4.0-SNAPSHOT"]
                   [lein-simpleton "1.1.0"]
                   [lein-try "0.4.3"]
-                  [lein-gorilla "0.3.6"]
-                  [cider/cider-nrepl "0.15.0-SNAPSHOT"]]
+                  [lein-gorilla "0.3.6"]]
         :dependencies []
         :repl-options {:prompt (fn [ns] (str "\u001B[35m[\u001B[34m" ns "\u001B[35m]\u001B[33mcλ:\u001B[m " ))
                        :welcome (println "Time for  REPL Driven Development!")}}}
