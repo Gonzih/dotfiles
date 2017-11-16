71a2276dcd26ce1871969e032245302c85c4bfe3
Max Gonzih <gonzih@gmail.com>
2017-11-16 14:26:36 +0100

    Lein cleanup

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,3 @@
-{:user {:plugins [[lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
-                  [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
-                  [lein-plz "0.4.0-SNAPSHOT"]
-                  [lein-simpleton "1.1.0"]
-                  [lein-try "0.4.3"]
-                  [lein-gorilla "0.3.6"]]
-        :dependencies []
-        :repl-options {:prompt (fn [ns] (str "\u001B[35m[\u001B[34m" ns "\u001B[35m]\u001B[33mcλ:\u001B[m " ))
-                       :welcome (println "Time for  REPL Driven Development!")}}}
+{:user {:plugins [[lein-ancient "0.6.14" :exclusions [org.clojure/clojure]]
+                  [cider/cider-nrepl "0.15.1"]]
+        :dependencies []}}
