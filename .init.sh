e1401b3bba71f88a7a92081f1f780cde7518d3d6
Max Gonzih <gonzih@gmail.com>
2019-04-12 13:35:46 -0400

    Fixing dotfiles crap

--- .init.sh
+++ .init.sh
@@ -4,12 +4,8 @@ set -e
 cd ~
 
 git clone git@github.com:Gonzih/dotfiles.git
-mv ~/dotfiles/* -t ~/ -f
-mv ~/dotfiles/.* -t ~/ -f
+mv $(find ~/dotfiles/) -t ~/ -f
 rm ~/dotfiles -rf
 git reset --hard
-
-test -e /usr/bin/ansible || (sudo pacman -Sy ansible --noconfirm)
-ansible-playbook provision.yml
 
 fish -c dotfiles-update
