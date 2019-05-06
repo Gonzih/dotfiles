1510bad689910ca2a258075de31ff55b95529b07
Max Gonzih <gonzih@gmail.com>
2019-05-05 22:27:27 -0400

    Add fix for screensaver and vlc on kde

---
+++ bin/disable_xsc.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# disable the X screensaver
+# The kde screensaver should still work, though
+# http://opensuse.14.x6.nabble.com/Disabling-display-power-management-td3039820.html
+
+xset s noblank -dpms
+
+exit 0
