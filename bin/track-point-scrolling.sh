8877aca1b88bd9c5f312ac6fb972ed91b4cf0d82
Max Gonzih <gonzih@gmail.com>
2020-11-05 23:11:37 -0500

    Something

---
+++ bin/track-point-scrolling.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 1
+xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 2
+xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 200
